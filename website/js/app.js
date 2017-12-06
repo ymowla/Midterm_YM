5a895c488b822ffa79acdd444359fbbfd68af611
ymowla <32875156+ymowla@users.noreply.github.com>
2017-12-06 14:26:55 -0500

    need to fix control

--- website/js/app.js
+++ website/js/app.js
@@ -15,3 +15,20 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// shows and hides filtered items
+$(".filter-simple-button").click(function() {
+  var value = $(this).attr('data-filter');
+  if(value === "all") {
+    $('.filter-simple-item').show('1000');
+  } else {
+    $(".filter-simple-item").not('.'+value).hide('3000');
+    $('.filter-simple-item').filter('.'+value).show('3000');
+  }
+});
+
+// changes active class on filter buttons
+$('.filter-simple-button').click(function () {
+  $(this).siblings().removeClass('is-active');
+  $(this).addClass('is-active');
+});
